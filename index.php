c57d18b46c9c0fb7d57a1a7853fb309fa6319b9e
Rizki <rizkikurniawan1797@gmail.com>
2020-12-06 14:38:39 +0700

    PHP :P

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<?php
+  //include 'connection.php';
+?>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
